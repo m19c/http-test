50c7952f61cc5247672e656e247747c824f488b3
Marc Binder <marc.binder@boerse-go.de>
2015-11-12 08:26:54 +0100

    Add possibile tests

--- test/util/validate.test.js
+++ test/util/validate.test.js
@@ -1,2 +1,5 @@
-describe('http-test/util/validate', function() {
+describe('http-test/util/validate', function validateTestSuite() {
+  it('marks the test as "invalid" if a error is provided');
+  it('marks the test as "failed" if the statusCode is not successful');
+  it('checks the given thresholds if provided');
 });
